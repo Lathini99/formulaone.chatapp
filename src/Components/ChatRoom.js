a26fde19246f8fa8dbfc1e219fab2f79abe82f00
Lathini Navoda <lathidilz2016@gmail.com>
2024-03-10 17:24:19 +0530

    update chatroom.js

--- src/Components/ChatRoom.js
+++ src/Components/ChatRoom.js
@@ -2,24 +2,25 @@ import {Col, Row } from "react-bootstrap";
 import MessageContainer from "./MesssageContainer";
 import SendMessageForm from "./SendMessageForm";
 
-const ChatRoom = ({ messages, sendMessage}) => <div>
-    <Row className = 'px-5 py-5'>
-        <Col sm= {10}>
-            <h2>ChatRoom</h2>
-        </Col>
-        <Col>
 
+const ChatRoom = ({ messages, sendMessage }) => (
+    <div>
+      <Row className="px-5 py-3">
+        <Col sm={12}>
+          <h2 className="text-center">Chat Room</h2>
         </Col>
-    </Row>
-    <Row className= 'px-5 py-5'>
-        <Col sm={10}>
-            <MessageContainer messages={messages}/>
+      </Row>
+      <Row className="px-5 py-3">
+        <Col sm={12}>
+          <div className="message-container">
+            <MessageContainer messages={messages} />
+          </div>
+          <div className="send-message-form">
+            <SendMessageForm sendMessage={sendMessage} />
+          </div>
         </Col>
-        <Col sm={10}>
-            <SendMessageForm sendMessage={sendMessage}/>
-        </Col>
-
-    </Row>
-</div>
-
-export default ChatRoom;
+      </Row>
+    </div>
+  );
+  
+  export default ChatRoom;
